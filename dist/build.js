19644591e5c44ba41fa89bcf0200338269879b49
Raymond <ray@firestitch.com>
2016-05-25 15:58:21 -0400

    commit

---
+++ dist/build.js
@@ -0,0 +1,46 @@
+(function () {
+    'use strict';
+
+    angular.module('fs-angular-build',[])
+    .service('fsBuild', function ($http, $timeout, fsModal) {
+        
+        var build_date = null;
+        var service = {
+            listen: listen
+        };
+
+        return service;
+
+        function listen(interval) {
+
+            var interval = interval || 60;
+
+            $http.get("build.json")
+            .then(function(build) {
+                if(build.data && build.data.build_date) {
+                    var latest_build_date = new Date(build.data.build_date);
+
+                    console.log('Latest Build Date: ' + latest_build_date);
+
+                    if(build_date!==null) {
+                        if(build_date<latest_build_date) {
+                            fsModal.confirm({ content: 'There is a new version of this app available. Refresh now?',
+                                                ok: function() {
+                                                    location.reload(true);
+                                                }});
+                        }
+                    }
+
+                    build_date = latest_build_date;
+                }
+            })
+            .catch(function(e) {
+
+            });
+
+            $timeout(function() {
+                listen();
+            },interval * 1000)
+        }
+    });
+})();
